5be572da6ea918550dd87d9fc2dbf444c5a520dc
SaydurRahmanSaied <saydurrahman3952@gmail.com>
2025-09-15 21:39:14 +0600

    pdf open js is added by C233183

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll("#card-container a[data-pdf]").forEach(item =>{
+    item.addEventListener("click", function(e) {
+        e.preventDefault();
+        const pdfFile = this.getAttribute("data-pdf");
+        if(pdfFile){
+            window.location.href = pdfFile;
+        }
+
+    })
+})
