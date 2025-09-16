c15594bf4caec05bf581031177423c52929abd83
SaydurRahmanSaied <saydurrahman3952@gmail.com>
2025-09-16 16:36:45 +0600

    all function added

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ document.querySelectorAll("#card-container a[data-pdf]").forEach((item) => {
   item.addEventListener("click", function (e) {
     e.preventDefault();
     const pdfFile = this.getAttribute("data-pdf");
+    // console.log("PDF file to open:", pdfFile);
     if (pdfFile) {
       window.location.href = pdfFile;
     }
   });
 });
+
+
